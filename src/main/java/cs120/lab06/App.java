e195bbf49158e90db9956305903fbc07fbeb3376
Andrew McInnis <Amcinnis22@austincollege.edu>
2026-05-12 17:43:31 -0500

    feat(02-03): attach styles.css to Scene and resize to 700x520
    - Change Scene dimensions from 640x480 to 700x520
    - Add scene.getStylesheets().add(App.class.getResource("styles.css").toExternalForm()) after scene creation
    - CSS attachment uses classpath-relative lookup (App.class in cs120.lab06, styles.css in same package resources)
    - mvn compile: BUILD SUCCESS

--- src/main/java/cs120/lab06/App.java
+++ src/main/java/cs120/lab06/App.java
@@ -17,7 +17,8 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("primary"), 700, 520);
+        scene.getStylesheets().add(App.class.getResource("styles.css").toExternalForm());
         stage.setTitle("TextTwist");
         stage.setScene(scene);
         stage.show();
